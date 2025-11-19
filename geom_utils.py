850edfa1f9518711f9c8da753e1816641c778b71
Sayed <sayedibrahim.co@gmail.com>
2025-11-19 23:54:42 +0200

    feat: fun(Splits a polygon and returns the half containing the reference point)

--- geom_utils.py
+++ geom_utils.py
@@ -56,3 +56,32 @@ def perpendicular_bisector(a, b, length=1000):
     p1= m+ length * np.array(p)
     p2= m - length * np.array(p)
     return LineString([tuple(np.round(p1, 3)), tuple(np.round(p2, 3))])
+
+from shapely.ops import split
+from shapely.geometry import Polygon, Point, LineString, GeometryCollection
+
+
+def half_plane_polygon(line: LineString, space: Polygon, point: Point) -> Polygon:
+    """
+    Splits a polygon and returns the half containing the reference point.
+    Args:
+        line (LineString): The line used for splitting.
+        space (Polygon): The polygon to be split.
+        point (Point): The reference point used to select the correct half.
+
+    Returns:
+        Polygon: The resulting polygon that contains the reference point.
+    """
+    parts = split(space, line)
+
+    if len(parts.geoms) == 1:
+        if parts.geoms[0].contains(point):
+            return parts.geoms[0]
+        else:
+            return space
+        
+    for poly in parts.geoms:
+        if poly.intersects(point):
+            return poly
+
+    raise ValueError("The reference point does not fall inside any of the polygons after the split")
